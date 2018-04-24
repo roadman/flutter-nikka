aed6f7808b8dfd8e0b7c718fcab7cbb26fe78618
roadman <roadmang@gmail.com>
2018-04-25 02:20:26 +0900

    change listview -> list

--- lib/todo_table.dart
+++ lib/todo_table.dart
@@ -6,81 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class Todo {
-  Todo(this.name, this.priority);
-  final String name;
-  final int priority;
-
-  bool selected = false;
-}
-
-class TodoDataSource extends DataTableSource {
-  final List<Todo> _todos = <Todo>[
-    new Todo('Sample todo', 0)
-  ];
-
-  void _sort<T>(Comparable<T> getField(Todo d), bool ascending) {
-    _todos.sort((Todo a, Todo b) {
-      if (!ascending) {
-        final Todo c = a;
-        a = b;
-        b = c;
-      }
-      final Comparable<T> aValue = getField(a);
-      final Comparable<T> bValue = getField(b);
-      return Comparable.compare(aValue, bValue);
-    });
-    notifyListeners();
-  }
-
-  int _selectedCount = 0;
-
-  @override
-  DataRow getRow(int index) {
-    assert(index >= 0);
-    if (index >= _todos.length)
-      return null;
-    final Todo todo = _todos[index];
-    return new DataRow.byIndex(
-      index: index,
-      selected: todo.selected,
-      onSelectChanged: (bool value) {
-        if (todo.selected != value) {
-          _selectedCount += value ? 1 : -1;
-          assert(_selectedCount >= 0);
-          todo.selected = value;
-          notifyListeners();
-        }
-      },
-      cells: <DataCell>[
-        new DataCell(new Text('${todo.name}')),
-        new DataCell(new Text('${todo.priority}')),
-      ]
-    );
-  }
-
-  @override
-  int get rowCount => _todos.length;
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get selectedRowCount => _selectedCount;
-
-  void _selectAll(bool checked) {
-    for (Todo todo in _todos)
-      todo.selected = checked;
-    _selectedCount = checked ? _todos.length : 0;
-    notifyListeners();
-  }
-
-  void _addRow() {
-    _todos.add(
-      new Todo('todo', 0)
-    );
-    notifyListeners();
-  }
-
+  Todo(this.selected, this.name, this.priority);
+  String name;
+  int priority;
+  bool selected;
 }
 
 class TodoTable extends StatefulWidget {
@@ -91,51 +20,41 @@ class TodoTable extends StatefulWidget {
 }
 
 class _DataTableDemoState extends State<TodoTable> {
-  int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
-  int _sortColumnIndex;
-  bool _sortAscending = true;
-  final TodoDataSource _todosDataSource = new TodoDataSource();
+  
+  final List<Todo> _todos = <Todo>[
+    new Todo(false, 'Sample todo', 0)
+  ];
 
-  void _sort<T>(Comparable<T> getField(Todo d), int columnIndex, bool ascending) {
-    _todosDataSource._sort<T>(getField, ascending);
-    setState(() {
-      _sortColumnIndex = columnIndex;
-      _sortAscending = ascending;
-    });
+  void _addRow() {
+    _todos.add(
+      new Todo(false, 'todo', 0)
+    );
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: const Text('Todo list')),
-      body: new ListView(
-        padding: const EdgeInsets.all(20.0),
-        children: <Widget>[
-          new PaginatedDataTable(
-            header: const Text('Todo'),
-            rowsPerPage: _rowsPerPage,
-            onRowsPerPageChanged: (int value) { setState(() { _rowsPerPage = value; }); },
-            sortColumnIndex: _sortColumnIndex,
-            sortAscending: _sortAscending,
-            onSelectAll: _todosDataSource._selectAll,
-            columns: <DataColumn>[
-              new DataColumn(
-                label: const Text('Todo (100g serving)'),
-                onSort: (int columnIndex, bool ascending) => _sort<String>((Todo d) => d.name, columnIndex, ascending)
+      body: new ListView.builder(
+        itemCount: _todos.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          return new Row(
+            children: <Widget>[
+              new Checkbox(
+                value: _todos[index].selected,
+                onChanged: (bool newValue) {
+                  _todos[index].selected = newValue;
+                  setState(() {});
+                }
               ),
-              new DataColumn(
-                label: const Text('Priority'),
-                tooltip: 'todo priority.',
-                numeric: true,
-                onSort: (int columnIndex, bool ascending) => _sort<num>((Todo d) => d.priority, columnIndex, ascending)
-              ),
+              new Text(_todos[index].name)
             ],
-            source: _todosDataSource
-          )
-        ]
+          );
+        }
       ),
       floatingActionButton: new FloatingActionButton(	
-        onPressed: _todosDataSource._addRow,
+        onPressed: _addRow,
         tooltip: 'Increment',	
         child: new Icon(Icons.add),	
       )
